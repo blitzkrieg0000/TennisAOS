8685fd05c61e77e0e4fec67325a0a961d6ba84be
blitzkrieg0000 <burakhansamli0.0.0.0@gmail.com>
2022-09-03 01:11:38 +0300

    Update StatusChecker.py

--- gRPC/Manager/StatusChecker.py
+++ gRPC/Manager/StatusChecker.py
@@ -5,15 +5,12 @@ import time
 from typing import Any
 
 from clients.Redis.redis_client import RedisCacheManager
+from libs.helpers import Converters
 from libs.base import AbstractHandler
 from libs.logger import logger
 
 
 INTERVAL = 3 # Second(s)
-
-def bytes2obj(bytes):
-    if bytes is not None or bytes != b'':
-        return pickle.loads(bytes)
 
 def timer(func):
     def wrapper(*args, **kwargs):
@@ -30,20 +27,16 @@ class StatusChecker(AbstractHandler):
 
     @timer
     def checkDatabase(self):
-        
-
         query_keys = ["process_id", "process_name", "session_id", "stream_id", "aos_type_id", "player_id", "court_id", "limit", "force","stream_name", "source", "court_line_array", "kafka_topic_name", "is_video"]
-        QUERY = f'''
-                    SELECT p.id, p.name, s.id, s.stream_id, s.aos_type_id, s.player_id, s.court_id, s."limit", s."force", st."name", st."source" , st.court_line_array, st.kafka_topic_name, st.is_video
-                    FROM public."Process" as p
-                    INNER JOIN public."SessionParameter" as s
-                    ON p.session_id = s.id
-                    INNER JOIN public."Stream" as st
-                    on st.id =  s.stream_id
-                    WHERE p.is_completed=false
-                '''
+        QUERY = f'SELECT p.id, p.name, s.id, s.stream_id, s.aos_type_id, s.player_id, s.court_id, s."limit", s."force", st."name", st."source" , st.court_line_array, st.kafka_topic_name, st.is_video\
+                FROM public."Process" as p\
+                INNER JOIN public."SessionParameter" as s\
+                ON p.session_id = s.id\
+                INNER JOIN public."Stream" as st\
+                on st.id =  s.stream_id\
+                WHERE p.is_completed=false'
         processData = self.rcm.Read(query=QUERY, force=True)
-        processes = bytes2obj(processData)
+        processes = Converters.bytes2obj(processData)
         if processes is not None:
             return [dict(zip(query_keys, process)) for process in processes]
         return []
